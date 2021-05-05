80e5c3a291e71110f09613b82833b385ab0a6bc2
Sarah89Pro <sarahproietti1@gmail.com>
2021-05-05 03:08:39 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <!--HEADER-->
+  <Header/>
+  <!--MAIN CONTENT-->
+
+  <!--FOOTER-->
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '../src/components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+/*GENERAL*/ 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+body {
+  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+
+a {
+  text-decoration: none;
+}
+
+
+ul {
+  list-style: none;
+}
+
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,130 @@
+<template>
+<header class="container">
+    <div class="img">
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <div class="menu">
+        <ul>
+            <li
+            v-for="(link, index) in links"
+            :key="index" :class="{active: link.active}" >
+            <a
+            :class="{selected: link.active}"
+            :href="link.url">{{ link.nome }}
+            </a>
+            </li>
+        </ul>
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            links: [
+                {
+                    nome: 'characters',
+                    url: '/characters',
+                    active: true
+                },
+                { 
+                    nome: 'comics',
+                    url: '/comics',
+                    active: false
+                },
+                { 
+                    nome: 'movies',
+                    url: '/movies',
+                    active: false
+                },
+                {
+                    nome: 'tv',
+                    url: '/tv',
+                    active: false
+                },
+                {
+                    nome: 'games',
+                    url: '/games',
+                    active: false
+                },
+                { 
+                    nome: 'collectibles',
+                    url: '/collectibles',
+                    active: false
+                },
+                { 
+                    nome: 'videos',
+                    url: '/videos',
+                    active: false
+                },
+                { 
+                    nome: 'fans',
+                    url: '/fans',
+                    active: false
+                },
+                { 
+                    nome: 'news',
+                    url: '/news',
+                    active: false
+                },
+                { 
+                    nome: 'shop',
+                    url: '/shop',
+                    active: false
+                },
+            ],
+        };
+    },
+}
+</script>
+
+<style scoped>
+
+header {
+    display: flex;
+    justify-content: space-between;
+    line-height: 120px;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 12px;
+}
+
+
+a {
+    color: #000;
+}
+
+
+.menu ul {
+    display: flex;
+    justify-content: center;
+}
+
+
+.menu li {
+    margin-left: 20px;
+}
+
+
+.selected {
+    color: rgb(45, 143, 241);
+}
+
+
+.active {
+    border-bottom: 5px solid rgb(45, 143, 241);
+}
+
+
+.img {
+    display: flex;
+    align-items: center;
+}
+
+
+.img img  {
+    max-width: 80px;
+}
+</style>
